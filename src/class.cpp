7dd44e8268f2be5c89ad3e57bb00ab0963d6ead1
jrtilak <iamjrtilak@gmail.com>
2024-07-14 11:34:15 +0545

    feat: add db functions

--- src/class.cpp
+++ src/class.cpp
@@ -1,27 +1,75 @@
 #include <string>
+#include <vector>
+#include <iostream>
+
 using namespace std;
-#define MAX_QUESTIONS 100
+
 class QuizQuestion
 {
   string question;
   string options[4];
-  int answerIndex;
   int correctOption;
+
+public:
+  QuizQuestion(){};
+  QuizQuestion(string question, string options[], int correctOption)
+  {
+    this->question = question;
+    for (int i = 0; i < 4; i++)
+    {
+      this->options[i] = options[i];
+    }
+    this->correctOption = correctOption;
+  }
+
+  string getQuestion()
+  {
+    return question;
+  }
+
+  string *getOptions()
+  {
+    return options;
+  }
+
+  int getCorrectOption()
+  {
+    return correctOption;
+  }
 };
 
 class Quiz
 {
 private:
   string topic;
-  // QuizQuestion questions[MAX_QUESTIONS]; // this will be dynamically allocated asking use the totalQuestions
+  vector<QuizQuestion> questions;
   int totalQuestions;
   int currentQuestionIndex;
-  int correctAnswersIndex;
-  int wrongAnswersIndex;
+  vector<int> correctAnswersIndex;
+  vector<int> wrongAnswersIndex;
   int score;
   int totalScore;
+  vector<int> usedQuestionsIndex;
 
 public:
+  Quiz()
+  {
+    this->topic = "";
+    this->totalQuestions = 0;
+    this->currentQuestionIndex = 0;
+    this->score = 0;
+    this->totalScore = 0;
+  };
+
+  Quiz(string topic, int totalQuestions)
+  {
+    this->topic = topic;
+    this->totalQuestions = totalQuestions;
+    this->currentQuestionIndex = 0;
+    this->score = 0;
+    this->totalScore = 0;
+    this->questions = getQuizDataFromDB(topic, totalQuestions);
+  };
 };
 
 class Analytic
--- src/db.cpp
+++ src/db.cpp
@@ -1,1 +1,66 @@
-// interacts with db to read write and parse data
+#include "./class.cpp"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "../libs/csv.hpp"
+using namespace csv;
+#include <string>
+
+using namespace std;
+using namespace csv;
+#define PATH_QUIZ "../db/quiz/"
+
+vector<QuizQuestion> getQuizDataFromDB(string topic, int totalQuestions)
+{
+  vector<QuizQuestion> questions;
+  string path = PATH_QUIZ + topic + ".csv";
+  // Initialize CSVReader with the path to the CSV file
+  CSVReader reader(path);
+
+  // Check if the file exists by attempting to open it
+  ifstream file(path);
+  if (!file.is_open())
+  {
+    cout << "Topic not found" << endl;
+    return questions; // Return the empty vector if file doesn't exist
+  }
+  file.close(); // Close the file as CSVReader will handle the file reading
+
+  // Iterate through each row of the CSV file
+  for (CSVRow &row : reader)
+  {
+    string options[4] = {row["Option1"].get<>(), row["Option2"].get<>(), row["Option3"].get<>(), row["Option4"].get<>()};
+    string correctOption = row["CorrectOption"].get<>();
+    string questionText = row["Question"].get<>();
+    QuizQuestion question(questionText, options, stoi(correctOption));
+
+    // Add the constructed question to the questions vector
+    questions.push_back(question);
+
+    // Optionally, break out of the loop once you've read the desired number of questions
+    if (questions.size() >= totalQuestions)
+      break;
+  }
+
+  return questions;
+}
+
+void saveQuizDataToDB(string topic, vector<QuizQuestion> questions)
+{
+  string path = PATH_QUIZ + topic + ".csv";
+  // Initialize CSVWriter with the path to the CSV file
+  CSV Writer(path);
+
+  // Write the header row
+  Writer << "Question" << "Option1" << "Option2" << "Option3" << "Option4" << "CorrectOption";
+
+  // Iterate through each question and write it to the CSV file
+  for (QuizQuestion &question : questions)
+  {
+    string *options = question.getOptions();
+    Writer << question.getQuestion() << options[0] << options[1] << options[2] << options[3] << question.getCorrectOption();
+  }
+
+  // Close the CSV file after writing
+  Writer.close();
+}
